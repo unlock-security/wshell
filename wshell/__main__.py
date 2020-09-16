5fcc5d8ac7c908f2b79b004f84607480149b3a4e
Francesco Marano <francesco.mrn24@gmail.com>
2020-09-17 00:19:28 +0200

    Add empty entry point
    This is just to write setup.py

---
+++ wshell/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""The main entry point. Invoke as `wshell' or `python3 -m wshell'."""
+
+
+def main():
+    print("Hello, WShell!")
+
+
+if __name__ == "__main__":
+    main()

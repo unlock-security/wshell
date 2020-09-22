f740e29cda56b6d3b51502b3d34280f0d2884d11
Francesco Marano <francesco.mrn24@gmail.com>
2020-09-22 10:14:23 +0200

    Add basic command loop functionality

---
+++ wshell/cli.py
@@ -0,0 +1,50 @@
+import cmd2
+from typing import Union
+
+from wshell import settings
+from wshell.injectors import LinuxCommandInjector, WindowsCmdCommandInjector, WindowsPshCommandInjector
+
+
+# noinspection PyMethodMayBeStatic,PyUnusedLocal
+class WShellCmd(cmd2.Cmd):
+    """ The wshell command loop shell """
+
+    # Disable cmd2.Cmd advanced features we do not need (for now)
+    delattr(cmd2.Cmd, "do__relative_run_script")
+    delattr(cmd2.Cmd, "do_run_pyscript")
+    delattr(cmd2.Cmd, "do_run_script")
+    delattr(cmd2.Cmd, "do_shortcuts")
+    delattr(cmd2.Cmd, "do_py")
+
+    def __init__(
+            self,
+            injector: Union[LinuxCommandInjector, WindowsCmdCommandInjector, WindowsPshCommandInjector],
+            command_prompt: str = settings.DEFAULT_COMMAND_PROMPT
+    ):
+        super().__init__(
+            allow_cli_args=False,     # To avoid using URL and HTTP parameters from the command line as commands
+            allow_redirection=False,  # Disable output redirection ('>', '>>' and '|') to forward it to the target
+            shortcuts={'?': 'help', '!': 'shell'}
+        )
+
+        self.injector = injector
+        self.prompt = command_prompt
+
+    def default(self, statement: cmd2.Statement) -> None:
+        """ In case the user typed a non-builtin command, send it to the target. """
+        self.history.append(statement)
+        cmd_output = self.injector.execute(cmd=statement.raw)
+        self.poutput(cmd_output, end='')
+
+    def do_exit(self, line) -> bool:
+        """ Exit from wshell """
+        return True
+
+    # noinspection PyPep8Naming
+    def do_EOF(self, line) -> bool:
+        """ Exit from wshell by pressing CTRL+D """
+        self.poutput("^D")
+        return True
+
+    def emptyline(self) -> bool:
+        """ Do nothing on empty command """

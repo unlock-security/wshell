374e4a94567383fc65bc02bc399c08f3fa71a4d7
Francesco Marano <francesco.mrn24@gmail.com>
2020-09-24 19:55:28 +0200

    Add some custom exceptions

---
+++ wshell/errors.py
@@ -0,0 +1,34 @@
+from wshell.status import ExitStatus
+
+
+class WShellError(Exception):
+    """ The base wshell error class """
+    EXIT_STATUS = ExitStatus.GENERIC_ERROR
+
+
+class InjectorError(WShellError):
+    """ The base class for injectors-related errors """
+
+
+class CommandExecutionError(InjectorError):
+    """ The command execution failed """
+    EXIT_STATUS = ExitStatus.ERROR_CANNOT_EXECUTE
+
+
+class OsDetectionError(InjectorError):
+    """ The automatic OS detection failed """
+    EXIT_STATUS = ExitStatus.ERROR_UNDETECTED_OS
+
+
+class HttpError(WShellError):
+    """ The base class for http-related errors """
+
+
+class TargetUnreachableError(HttpError):
+    """ The target is unreachable, cannot connect """
+    EXIT_STATUS = ExitStatus.ERROR_TARGET_UNREACHABLE
+
+
+class TimeoutExpiredError(HttpError):
+    """ The timeout for connect/read/write is expired """
+    EXIT_STATUS = ExitStatus.ERROR_TIMEOUT_EXPIRED

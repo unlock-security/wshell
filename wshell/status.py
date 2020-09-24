2a2d022c2fbf1250949ce186c7d65cf30caba9a7
Francesco Marano <francesco.mrn24@gmail.com>
2020-09-24 19:54:57 +0200

    Add some useful exit status codes

--- wshell/status.py
+++ wshell/status.py
@@ -4,7 +4,18 @@ from enum import IntEnum, unique
 @unique
 class ExitStatus(IntEnum):
     """ Program exit status code constants """
+
+    # Generic
     SUCCESS = 0
     GENERIC_ERROR = 1
 
+    # Injectors
+    ERROR_CANNOT_EXECUTE = 10
+    ERROR_UNDETECTED_OS = 11
+
+    # HTTP
+    ERROR_TARGET_UNREACHABLE = 20
+    ERROR_TIMEOUT_EXPIRED = 21
+
+    # Signals
     ERROR_CTRL_C = 130

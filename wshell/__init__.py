8ca11cb1cfe48a6564ae98d1f1b51223c07f058e
Francesco Marano <francesco.mrn24@gmail.com>
2020-09-29 10:44:14 +0200

    Add DATA_DIR variable to point to the data/ directory

--- wshell/__init__.py
+++ wshell/__init__.py
@@ -2,5 +2,9 @@
 WShell: Turn a web-based {code,command,template} injection in a full featured shell with ease.
 """
 
+import os
+
 __version__ = '0.1.0'
 __author__ = 'Francesco Marano (@mrnfrancesco)'
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')

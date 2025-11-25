c31b46c2acb30a3988b3b27504264e37c631b052
Francesco Marano <francesco.marano@unlock-security.it>
2025-11-25 13:42:24 +0100

    Improve input script docstring

--- wshell/scripts/output/unescape.py
+++ wshell/scripts/output/unescape.py
@@ -1,3 +1,3 @@
 def run(output: str) -> str:
-    """ Unescape output (e.g. replace \\r and \\n with newlines) """
+    """ Slash unescape output (e.g. replace \\r and \\n with newlines) """
     return output.encode("latin-1", "backslashreplace").decode("unicode-escape")

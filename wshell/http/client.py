ed59b642065f1a59dcccab7b4d7c621883abe208
Francesco Marano <francesco.marano@unlock-security.it>
2025-12-02 18:17:52 +0100

    Create separate class for HTTP client

---
+++ wshell/http/client.py
@@ -0,0 +1,58 @@
+"""
+HTTP Client for WShell.
+"""
+from __future__ import annotations
+
+import time
+from typing import Any, Dict, Optional
+
+import requests
+import urllib3
+
+from wshell.config import Config
+from wshell.errors import TargetUnreachableError
+
+# Disable warnings related to unverified SSL certs
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class HttpClient:
+    """
+    A dedicated HTTP client for WShell, encapsulating requests logic.
+    """
+    def __init__(self, config: Config):
+        self.http = requests.Session() if config.reuse_connection else requests
+        self.allow_redirects = config.allow_redirects
+        self.timeout = config.timeout
+        self.delay = config.delay
+        self.verify = False
+        
+        requests.utils.default_user_agent = lambda: config.user_agent
+
+    def send_request(
+        self,
+        method: str,
+        url: str,
+        headers: Optional[Dict[str, str]] = None,
+        data: Optional[Dict[str, Any]] = None,
+        json: Optional[Dict[str, Any]] = None
+    ) -> requests.Response:
+        """
+        Sends an HTTP request.
+        """
+        time.sleep(self.delay)
+
+        try:
+            response = self.http.request(
+                method,
+                url,
+                headers=headers,
+                data=data,
+                json=json,
+                allow_redirects=self.allow_redirects,
+                timeout=self.timeout,
+                verify=self.verify,
+            )
+            return response
+        except requests.exceptions.ConnectionError as e:
+            raise TargetUnreachableError(e)
